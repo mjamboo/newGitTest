977f3835c8a9eead21aee6156a936f0c365907dd
mjamboo <mohammed.jamboo@orderdynamics.com>
2015-03-31 12:01:03 +0530

    Made changes to index.php and added index2.php

--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php
  
     $hmac_key = "speedo3Pow4Tag";
-    $body = 'Mohammed Jamboo';
+    $body = '';
     $calculatedHmac = base64_encode(hash ('sha256', $hmac_key . $body, true));
 	print($calculatedHmac);
 ?>
